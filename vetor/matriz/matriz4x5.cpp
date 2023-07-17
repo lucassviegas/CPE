c862115361e7f968a1495512158f5708f6f78ddf
lucas viegas <90990096+lucasviegas1017@users.noreply.github.com>
2023-07-17 11:34:54 -0300

    Create matriz4x5.cpp

---
+++ vetor/matriz/matriz4x5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+ using namespace std;
+ int main()
+ {
+     int soma=0;
+     const int L=4;
+     const int C=5;
+     int i, j;
+     int m[L][c];
+     int vetor[5];
+     for (i=0;i<L;i++){
+         for (j=0;j<<jj++){
+             cin>>m[i][j];
+     for (j=0;j<43;j++)(
+         for (i=0;i<L;i++){
+            soma=soma+m(i)[j];
+            vetor[j] ]=soma;
+       soma=0;
+     for (i=0;i<5;i++){
+        cout<<<coluna "<<i<<": "<<vetor[i]<<"\n";
+     }
+     return 0;
+     }
