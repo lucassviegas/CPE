444aff939545f8ef3a6d8b2d1f69c3b58fa95722
lucas viegas <90990096+lucasviegas1017@users.noreply.github.com>
2023-07-17 11:36:04 -0300

    Create t2.cpp

---
+++ vetor/matriz/t2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+     int n;
+    cout<<<Escolha o numero que ira definir a matriz quadradas";
+     cin>>n;
+     int i, j;
+     const int L=n;
+     const int C=n;
+     int m[L][C];
+    int T[L][C];
+    int P[L][C];
+    for(i=0; i<L; i++){
+         for(j=0;j<c;j++)
+              cout<<"o elemento "<<i<<";"""<<j<<"In";
+              cin>>m[i][j];
+          }
+     }
+           P[i][j]=m[i][j] []];
+    }
+   cout<<"\n";
+   for(i=0; i<L;i++{
+       for(j=0;j<Cjj++H
+           cout<<P[i][j]<<"It";
+        }
+       cout<<"\n";
+     return
+            0;
