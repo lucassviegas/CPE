00a9e5ae42290c9e510ffb81956e06d608e375ac
lucas viegas <90990096+lucasviegas1017@users.noreply.github.com>
2023-07-17 11:37:31 -0300

    Create matriz_diagonal.cpp

---
+++ vetor/matriz/matriz_diagonal.cpp
@@ -0,0 +1,28 @@
+using namespace std;
+int main()
+{
+     int n;
+    cout<<<Escolha O numero que ira definir a matriz quadradas";
+    cin>>n;
+     const int L=n;
+     const int C=n;
+     int i=0;
+     int j=0;
+     float m[L][c];
+     int menor;
+    for (i=0; i<n; i++)
+          for (j=0;j<n;j++){
+              cout<<"o elemento
+             cin>>m[i][j];
+          }
+     }
+     for (j-n; j>0;j--){
+        for(i=0;i<1;i**){
+             if(i==0){
+                menor=m[i][j];
+             }
+        }
+     }
+       cout<<"o menor componente diagonal: "<<menor;
+     return 0;
+        }
