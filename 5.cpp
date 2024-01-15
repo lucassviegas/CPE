b156c269504537e717ef59cf294901a0348491aa
lucas viegas <90990096+lucasviegas1017@users.noreply.github.com>
2024-01-15 12:11:46 -0300

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,32 @@
+#1nclude <1lostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+
+{
+
+setlocale(LC_ALL, "pt_BR.UTF-8");
+int n,
+int i=e;
+int x;
+int maior=0;
+int soma;
+int par
+cout<<"digite 5 numeros n inteiros: \n"3;
+cin>>n;
+do{
+cout<<"digite os numeros inteiros";
+cin>>x;
+a
+}while(i!=0);
+if(n>>10| |n>>0);
+5 To) eee ea eee
+cout<<"seu numero e par ou maior que 10
+y
+
+ina a oy
+
+oat
+
